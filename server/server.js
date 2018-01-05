6b4a1c649dbd8c925f474075470fdb7f43845592
Yarden Sibony <yardensibonyy@gmail.com>
2018-01-05 20:59:37 +0200

    setup prod cuild and server

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Server is Up!');
     console.log(__dirname);
 });
